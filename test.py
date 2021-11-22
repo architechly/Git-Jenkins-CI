9286234ac4683bd3ebd389c23a0e2813c3f01e73
Hari Prasad Kuppuraj <hari.pk@hotmail.com>
2021-11-22 18:13:34 +0800

    test

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ class TestHello(unittest.TestCase):
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
-        print("abadea")
+        print("abaadea")
     def test_hello(self):
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
